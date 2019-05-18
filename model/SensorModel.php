869fe8f69dabb5d1a9bacefcb9639dbb1242f3b7
Tenzin <tenzin.lote@hotmail.com>
2019-05-18 23:03:23 +0200

    issue#28 monitor pagina
    As a user i want to see all average analysis chart for the control page because that will give the user easy view

---
+++ model/SensorModel.php
@@ -0,0 +1,35 @@
+<?php
+
+class SensorClass{
+    private $dbIpHost = "localhost";
+    private $dbname = "mi40db";
+    private $dbHostName = "root";
+    private $dbPassword = "";
+    private $con;
+    //connectie make met de database
+    
+    public function __construct(){
+        $this->con = mysqli_connect($this->dbIpHost,$this->dbHostName,$this->dbPassword,$this->dbname);
+    }
+    
+    public function TemperatureSensor($sql1){
+        $result1 = mysqli_query($this->con,$sql1);
+        $temp = mysqli_fetch_array($result1);
+        return $temp;
+    }
+    
+    public function HumiditySensor($sql2){
+        $result2 = mysqli_query($this->con,$sql2);
+        $humi = mysqli_fetch_array($result2);
+        return $humi;
+    }
+    
+    public function BrightnessSensor($sql3){
+        $result3 = mysqli_query($this->con,$sql3);
+        $bright = mysqli_fetch_array($result3);
+        return $bright;
+    }
+    
+}
+
+?>
---
+++ view/monitorpageview.php
@@ -0,0 +1,75 @@
+<?php
+            include_once 'model/SensorModel.php';
+            $SensorModel = new SensorClass();
+            $sql1 = "SELECT * FROM temperature";
+            $sql2 = "SELECT * FROM humidity ORDER BY NowTimeDate DESC LIMIT 1";
+            $sql3 = "SELECT * FROM brightness";
+            $temp = $SensorModel-> TemperatureSensor($sql1);
+            $humi = $SensorModel-> HumiditySensor($sql2); 
+            $bright = $SensorModel-> BrightnessSensor($sql3);
+?>
+  
+   <html>
+    <head>
+<link rel="stylesheet" type="text/css" href="view/css/MisluktStyle.css"> 
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>HomePage</title>
+</head>
+
+<body>
+
+<div class="topnav">
+  <a class="active" href="javascript:location.reload(true)">Monitor</a>
+  <a href="#Control">Control</a>
+  <a href="#">Ground Plan</a>
+  <a href="start.php?page=contactpagecontroller">Contact</a>
+</div>
+
+<div class="Row">
+    <div class="Column">
+    Average Temparature (degrees)
+    <?php
+    
+        echo "<ul>";
+        echo "<li> Floor 1: ".$temp['TempFloor1']."°</li>";
+        echo "<li> Floor 2: ".$temp['TempFloor2']."°</li>";
+        echo "<li> Floor 3: ".$temp['TempFloor3']."°</li>";
+        echo "<li> Floor 4: ".$temp['TempFloor4']."°</li>";
+        echo "<li> Floor 5: ".$temp['TempFloor5']."°</li>";
+        echo "<li> Floor 6: ".$temp['TempFloor6']."°</li>";
+        echo "</ul>";
+    
+    ?>
+    </div>
+    <div class="Column">
+    Average Humidity
+    <?php
+    echo "<ul>";
+        echo "<li> Floor 1: ".$humi['HumiFloor1']."%</li>";
+        echo "<li> Floor 2: ".$humi['HumiFloor2']."%</li>";
+        echo "<li> Floor 3: ".$humi['HumiFloor3']."%</li>";
+        echo "<li> Floor 4: ".$humi['HumiFloor4']."%</li>";
+        echo "<li> Floor 5: ".$humi['HumiFloor5']."%</li>";
+        echo "<li> Floor 6: ".$humi['HumiFloor6']."%</li>";
+        echo "</ul>";
+        
+    ?>
+    </div>
+    <div class="Column">
+    Average Brightness (lumen)
+   <?php
+     echo "<ul>";
+        echo "<li> Floor 1: ".$bright['BrightFloor1']."L</li>";
+        echo "<li> Floor 2: ".$bright['BrightFloor2']."L</li>";
+        echo "<li> Floor 3: ".$bright['BrightFloor3']."L</li>";
+        echo "<li> Floor 4: ".$bright['BrightFloor4']."L</li>";
+        echo "<li> Floor 5: ".$bright['BrightFloor5']."L</li>";
+        echo "<li> Floor 6: ".$bright['BrightFloor6']."L</li>";
+        echo "</ul>";
+    ?>
+    </div>
+</div>
+
+</body>
+    
+</html>
